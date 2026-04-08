f7da281f720e52f19069d3784777f5799e37f87d
Aarushi123-bit <aarushidash22@gmail.com>
2026-04-08 17:50:50 +0530

    uc1

--- src/com/bookmystay/Main.java
+++ src/com/bookmystay/Main.java
@@ -3,10 +3,10 @@ package com.bookmystay;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("=====================================");
-        System.out.println("   Welcome to Book My Stay App   ");
-        System.out.println("=====================================");
+        System.out.printf("=====================================");
+        System.out.printf("   Welcome to Book My Stay App   ");
+        System.out.printf("=====================================");
 
-        System.out.println("Your smart hotel booking assistant!");
+        System.out.printf("Your smart hotel booking assistant!");
     }
 }
